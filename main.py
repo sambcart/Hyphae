23d5471f2d2c9313301b8a0ebc949a742e046724
Bryce Carter <bryce.carte@gmail.com>
2016-08-09 23:52:51 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import random
 import time
 
 if __name__ == "__main__":
@@ -8,34 +9,35 @@ if __name__ == "__main__":
     from modules.render import Render
     from modules.collision import CollisionDetectionGrid as CDG
 
-    size = 8000
-    render = Render(size, size, 0.15, 1.0)
+    size = 4000
+    init_rad = 20
+    boundary_rad = 1600
+    node_num = 5
 
-    default_node_kwargs = {
-        "pos": (0, 0),
-        "radius": size * 0.0125,
-        "angle": math.pi / 2,
-        "split_rate": 0.35,
-        "branch_rate": 0.05,
-        "single_child_rad_rat": 0.99,
-        "split_child_rad_rat": 0.94,
+    render = Render(size, size, 1.0, 0.15)
+
+    node_kwargs = lambda: {
+        "pos": random_point_rad(boundary_rad, 0.8),
+        "angle": random_angle(),
+        "radius": init_rad,
+        "split_rate": 0.25,
+        "branch_rate": 0.25,
+        "single_child_rad_rat": 0.98,
+        "split_child_rad_rat": 0.96,
         "branch_child_rad_rat": 0.75,
         "single_child_angle_sigma": math.pi / 12,
         "split_child_angle_dev": math.pi / 6,
         "branch_child_angle_dev": math.pi / 3,
-        "child_split_k": 1.05,
-        "child_branch_k": 1.075
+        "child_split_k": 1.025,
+        "child_branch_k": 1.01
     }
 
-    node_kwargs = handle_cli_input(default_node_kwargs)
-
-    default_node = Node(**node_kwargs)
-    default_cdg = CDG(size, default_node.radius)
+    default_cdg = CDG(size, init_rad)
 
     root_kwargs = {
-        "node": default_node,
-        "min_rad": 8,
-        "boundary_rad": size * 0.4,
+        "nodes": [Node(**node_kwargs()) for _ in xrange(node_num)],
+        "min_rad": 1,
+        "boundary_rad": boundary_rad,
         "min_x": -size/2,
         "max_x":  size/2,
         "min_y": -size/2,
@@ -44,7 +46,14 @@ if __name__ == "__main__":
     }
 
     root = Root(**root_kwargs)
-    root.generate()
-    root.draw(render)
 
-    render.save_png("hyphae-{}.png".format(int(time.time())))
+    try:
+        root.generate_frames(render)
+
+        #root.generate()
+        #root.draw(render)
+        #render.save_png("img/hyphae-{}.png".format(int(time.time())), verbose=True)
+
+    except KeyboardInterrupt:
+        root.draw(render)
+        render.save_png("img/hyphae-{}.png".format(int(time.time())), verbose=True)
