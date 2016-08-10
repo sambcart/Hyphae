e38428603d6be1d381c69ec1dcdf32f2c8467642
Bryce Carter <bryce.carte@gmail.com>
2016-08-09 23:57:51 -0400

    Update utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,10 +1,21 @@
 import math
+import random
 
 def loc_gamma(a, b, c):
     return math.acos((c*c - a*a - b*b) / (-2.*a*b))
 
 def loc_c(a, b, gamma):
     return math.sqrt(a*a + b*b - 2*a*b*math.cos(gamma))
+
+def random_point_rad(rad, buffer):
+    x = random.randint(-rad * buffer, rad * buffer)
+    ymax = int(math.sqrt(buffer * buffer * rad * rad - x * x))
+    ymin = -ymax
+    y = random.randint(ymin, ymax)
+    return (x, y)
+
+def random_angle():
+    return random.random() * 2 * math.pi
 
 def handle_cli_input(default_kwargs):
     import sys
